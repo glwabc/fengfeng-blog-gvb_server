3b5ce6765bce339d60259206f953cd8374ef424c
fengfeng <fengfengzhidao@126.com>
2023-02-14 23:08:15 +0800

    配置修改

--- api/settings_api/settings_info.go
+++ api/settings_api/settings_info.go
@@ -2,9 +2,10 @@ package settings_api
 
 import (
 	"github.com/gin-gonic/gin"
+	"gvb_server/global"
 	"gvb_server/models/res"
 )
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	res.FailWithCode(2, c)
+	res.OkWithData(global.Config.SiteInfo, c)
 }
