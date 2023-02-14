6eb70aec893e3481f96d392bfe6ffd32957a0779
fengfeng <fengfengzhidao@126.com>
2023-02-14 23:33:20 +0800

    多配置接口修改，合并为一个

--- api/settings_api/settings_update.go
+++ api/settings_api/settings_update.go
@@ -8,19 +8,61 @@ import (
 	"gvb_server/models/res"
 )
 
+// SettingsInfoUpdateView 修改某一项的配置信息
 func (SettingsApi) SettingsInfoUpdateView(c *gin.Context) {
-	var cr config.SiteInfo
-	err := c.ShouldBindJSON(&cr)
+	var cr SettingsUri
+	err := c.ShouldBindUri(&cr)
 	if err != nil {
 		res.FailWithCode(res.ArgumentError, c)
 		return
 	}
-	global.Config.SiteInfo = cr
-	err = core.SetYaml()
-	if err != nil {
-		global.Log.Error(err)
-		res.FailWithMessage(err.Error(), c)
+	switch cr.Name {
+	case "site":
+		var info config.SiteInfo
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.SiteInfo = info
+
+	case "email":
+		var info config.Email
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.Email = info
+	case "qq":
+		var info config.QQ
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.QQ = info
+	case "qiniu":
+		var info config.QiNiu
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.QiNiu = info
+	case "jwt":
+		var info config.Jwy
+		err = c.ShouldBindJSON(&info)
+		if err != nil {
+			res.FailWithCode(res.ArgumentError, c)
+			return
+		}
+		global.Config.Jwy = info
+	default:
+		res.FailWithMessage("没有对应的配置信息", c)
 		return
 	}
+
+	core.SetYaml()
 	res.OkWith(c)
 }
