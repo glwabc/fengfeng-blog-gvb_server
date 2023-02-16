5c0761126c6c96e8c0704bb869eaa127233d867b
fengfeng <fengfengzhidao@126.com>
2023-02-17 01:07:12 +0800

    添加菜单

--- routers/images_router.go
+++ routers/images_router.go
@@ -5,6 +5,7 @@ import "gvb_server/api"
 func (router RouterGroup) ImagesRouter() {
 	app := api.ApiGroupApp.ImagesApi
 	router.GET("images", app.ImageListView)
+	router.GET("image_names", app.ImageNameListView)
 	router.POST("images", app.ImageUploadView)
 	router.DELETE("images", app.ImageRemoveView)
 	router.PUT("images", app.ImageUpdateView)
