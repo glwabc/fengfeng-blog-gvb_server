835b0f111581b2ffd3bfeb72ed9b1e5c0c1fe67b
fengfeng <fengfengzhidao@126.com>
2023-02-18 18:11:15 +0800

    中间件

--- routers/user_router.go
+++ routers/user_router.go
@@ -1,8 +1,12 @@
 package routers
 
-import "gvb_server/api"
+import (
+	"gvb_server/api"
+	"gvb_server/middleware"
+)
 
 func (router RouterGroup) UserRouter() {
 	app := api.ApiGroupApp.UserApi
 	router.POST("email_login", app.EmailLoginView)
+	router.GET("users", middleware.JwtAuth(), app.UserListView)
 }
