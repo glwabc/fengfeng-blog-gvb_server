1d7a7e9bc74ac3cff4d305af5ee6d9dbcf66bd71
fengfeng <fengfengzhidao@126.com>
2023-02-18 23:04:59 +0800

    qq登录

--- routers/enter.go
+++ routers/enter.go
@@ -15,7 +15,6 @@ func InitRouter() *gin.Engine {
 	gin.SetMode(global.Config.System.Env)
 	router := gin.Default()
 	router.GET("/swagger/*any", gs.WrapHandler(swaggerFiles.Handler))
-
 	apiRouterGroup := router.Group("api")
 
 	routerGroupApp := RouterGroup{apiRouterGroup}
