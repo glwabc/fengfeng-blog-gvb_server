74ece271c5dcbe83ae2ab79fedbdceefad7fa6c2
fengfeng <fengfengzhidao@126.com>
2023-02-19 12:42:36 +0800

    消息列表

--- routers/message_router.go
+++ routers/message_router.go
@@ -2,9 +2,12 @@ package routers
 
 import (
 	"gvb_server/api"
+	"gvb_server/middleware"
 )
 
 func (router RouterGroup) MessageRouter() {
 	app := api.ApiGroupApp.MessageApi
 	router.POST("messages", app.MessageCreateView)
+	router.GET("messages_all", app.MessageListAllView)
+	router.GET("messages", middleware.JwtAuth(), app.MessageListView)
 }
