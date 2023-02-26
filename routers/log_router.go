7ec924f27a47b33a799241a2930c6d1586f8b8bc
fengfeng <fengfengzhidao@126.com>
2023-02-26 15:29:46 +0800

    日志删除完成

--- routers/log_router.go
+++ routers/log_router.go
@@ -1,8 +1,12 @@
 package routers
 
-import "gvb_server/api"
+import (
+	"gvb_server/api"
+	"gvb_server/middleware"
+)
 
 func (router RouterGroup) LogRouter() {
 	app := api.ApiGroupApp.LogApi
 	router.GET("logs", app.LogListView)
+	router.DELETE("logs", middleware.JwtAdmin(), app.LogRemoveListView)
 }
