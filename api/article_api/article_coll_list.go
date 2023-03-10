cd87d41ca591b7a36d5159b99ab84000c297739b
fengfeng <fengfengzhidao@126.com>
2023-03-11 01:07:47 +0800

    用户收藏的文章列表

--- api/article_api/article_coll_list.go
+++ api/article_api/article_coll_list.go
@@ -71,6 +71,7 @@ func (ArticleApi) ArticleCollListView(c *gin.Context) {
 			continue
 		}
 		article.ID = hit.Id
+		article.Content = ""
 		collList = append(collList, CollResponse{
 			ArticleModel: article,
 			CreatedAt:    collMap[hit.Id],
